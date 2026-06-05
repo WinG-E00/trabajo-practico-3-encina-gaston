abb2e424a85bc898e647afde63a4690eec6cbee1
WinjeKN <gastonencinawj@gmail.com>
2026-06-05 11:52:51 -0300

    main:comentarios con informacion y inclusion de cdn de bootstrap

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// Api de todos los personajes https://thesimpsonsapi.com/api/characters
+// Api de un personaje https://thesimpsonsapi.com/api/characters/1/
+
+
+const urlDeMiApi =  
+
+
+const obtenerPersonaje = async () => { 
+    try {
+        const response = await fetch(urlDeMiApi);
+        const dataResponse = response.json();
+
+
+        return dataResponse;
+
+    }
+    catch {
+        console.log("Error en mi promesa")
+    }
+};
